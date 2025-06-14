56e8d4e761fb90645c76dec51bf74e858904e98b
Guilherme Waskievicz <guilherme.waskievicz@gmail.com>
2025-06-14 14:11:13 -0300

    alterações parciais antes da reestruturação

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx:alpine
+COPY index.html /usr/share/nginx/html/
+COPY includes/ /usr/share/nginx/html/includes/
--- includes/js/main.js
+++ includes/js/main.js
@@ -49,15 +49,18 @@ function getUserDebug2() {
     });
 }
 
-function atualiza_tabela_variaveis(data){
+function atualiza_tabela_variaveis(data) {
     let $table = $('#tabela_variaveis');
     let $tbody = $('<tbody></tbody>');
     let contador = 0;
-    $.each(data, function(key, value) {
+    $.each(data, function (key, value) {
         let $row = $('<tr></tr>');
-        let $contador = $('<td></td>').text(contador).css({'text-align':'center',  'font-weight': 'bold'});
+        let $contador = $('<td></td>').text(contador).css({'text-align': 'center', 'font-weight': 'bold'});
         let $cellKey = $('<td></td>').text(key);
-        let $cellValue = $('<td></td>').text(value[value.length - 1]).css({'text-align':'right', 'padding-right': '15px'});
+        let $cellValue = $('<td></td>').text(value[value.length - 1]).css({
+            'text-align': 'right',
+            'padding-right': '15px'
+        });
 
         $row.append($contador);
         $row.append($cellKey);
@@ -70,18 +73,18 @@ function atualiza_tabela_variaveis(data){
     $table.append($tbody);
 }
 
-function carrega_historico_variaveis(){
+function carrega_historico_variaveis() {
     $("#modal_historico_variaveis").modal('show');
 
     let primeira_vez = true;
     let maior_colspan = 0;
     // verifica quantidade de colspans necessários
-    $.each(dic_control.historico_variavel, function(key, value) {
-        if (primeira_vez){
+    $.each(dic_control.historico_variavel, function (key, value) {
+        if (primeira_vez) {
             maior_colspan = value.length;
             primeira_vez = false;
         }
-        if (value.length > maior_colspan){
+        if (value.length > maior_colspan) {
             maior_colspan = value.length;
         }
     });
@@ -89,20 +92,20 @@ function carrega_historico_variaveis(){
     let $table = $('#tabela_historico_variaveis_modal');
     let $tbody = $('<tbody></tbody>');
     let contador = 0;
-    $.each(dic_control.historico_variavel, function(key, value) {
+    $.each(dic_control.historico_variavel, function (key, value) {
         let conta_celulas_criadas = maior_colspan;
         let $row = $('<tr></tr>');
-        let $contador = $('<td></td>').text(contador).css({'text-align':'center',  'font-weight': 'bold'});
+        let $contador = $('<td></td>').text(contador).css({'text-align': 'center', 'font-weight': 'bold'});
         let $cellKey = $('<td></td>').text(key);
         $row.append($contador);
         $row.append($cellKey);
-        for (let i=0;i<value.length;i++){
-            let $cellValue = $('<td></td>').text(value[i]).css({'text-align':'right', 'padding-right': '15px'});
+        for (let i = 0; i < value.length; i++) {
+            let $cellValue = $('<td></td>').text(value[i]).css({'text-align': 'right', 'padding-right': '15px'});
             $row.append($cellValue);
             conta_celulas_criadas--;
         }
 
-        while (conta_celulas_criadas>0){
+        while (conta_celulas_criadas > 0) {
             let $cellValue = $('<td></td>');
             $row.append($cellValue);
             conta_celulas_criadas--;
@@ -122,48 +125,66 @@ function carrega_historico_variaveis(){
 
 function modifica_cor_linhas_editor_texto(linha_atual, linha_anterior) {
     addLineDecoration(linha_atual - 1, 'line-decoration');
-    editor.scrollIntoView({ line: linha_atual-1, ch: 0 }, 200); // O segundo parâmetro é o tempo de animação em milissegundos (opcional)
+    editor.scrollIntoView({line: linha_atual - 1, ch: 0}, 200); // O segundo parâmetro é o tempo de animação em milissegundos (opcional)
     editor.removeLineClass(linha_anterior - 1, 'wrap', 'line-decoration');
 }
 
 
+var dic_control = {
+    'c3e': '',
+    'historico_variavel': {}
+};
 
-var dic_control = {'c3e': '',
-                                                 'historico_variavel': {}};
 
+function inicia_worker(debug = false) {
+    var worker = new Worker('includes/js/worker.js', {type: 'module'});
+    let $table = $('#tabela_variaveis');
+    $table.find('tbody').remove();
+    esta_no_debug = false;
+    textareaElement.value = "";
+    saveDataAndReload(false);
+    $("#button2")[0].hidden = true;
+    $("#button3")[0].hidden = false;
+    $("#button4")[0].hidden = true;
+    editor.setOption("readOnly", true);
+    if (debug) {
+        $("#button5")[0].hidden = false;
+        $("#button6")[0].hidden = false;
+        debug_compiler = lista_breakpoints;
+    } else {
+        $("#button5")[0].hidden = true;
+        $("#button6")[0].hidden = true;
+        debug_compiler = false;
+    }
+    mostra_tela_aguarde('Compilando...');
+    worker.postMessage({code: editor.getValue(), debug: debug_compiler});
 
+    const clientId = localStorage.getItem('clientId');
+    fetch('http://localhost:8088/api/compilar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({clientId: clientId})
+    })
+        .then(response => response.json())
+        .then(data => {
+            esconde_tela_aguarde();
+            document.getElementById('displayText').value = data.resultado;
+        })
+        .catch(error => {
+            esconde_tela_aguarde();
+            console.error('Erro na API:', error);
+        });
 
-function inicia_worker(debug=false){
-  var worker= new Worker('includes/js/worker.js', { type: 'module' });
-  let $table = $('#tabela_variaveis');
-  $table.find('tbody').remove();
-  esta_no_debug = false;
-  textareaElement.value = "";
-  saveDataAndReload(false);
-  $("#button2")[0].hidden = true;
-  $("#button3")[0].hidden = false;
-  $("#button4")[0].hidden = true;
-  editor.setOption("readOnly", true);
-  if (debug){
-      $("#button5")[0].hidden = false;
-      $("#button6")[0].hidden = false;
-      debug_compiler = lista_breakpoints;
-  } else {
-      $("#button5")[0].hidden = true;
-      $("#button6")[0].hidden = true;
-      debug_compiler = false;
-  }
-  mostra_tela_aguarde('Compilando...');
-  worker.postMessage({ code: editor.getValue(), debug: debug_compiler});
+    // Ouvir a resposta do Worker
+    worker.onmessage = async function (event) {
 
-  // Ouvir a resposta do Worker
-    worker.onmessage = async function(event) {
-
-        if ('saida_console' in event.data){
+        if ('saida_console' in event.data) {
             textareaElement.value += event.data.saida_console;
             textareaElement.scrollTop = textareaElement.scrollHeight;
         }
-        if ('finalizou_execucao' in event.data){
+        if ('finalizou_execucao' in event.data) {
             $("#button4")[0].hidden = false;
             $("#button5")[0].hidden = true;
             $("#button6")[0].hidden = true;
@@ -172,28 +193,28 @@ function inicia_worker(debug=false){
             $("#inputText")[0].disabled = true;
             editor.setOption("readOnly", false);
             textareaElement.scrollTop = textareaElement.scrollHeight;
-            dic_control.c3e =  event.data.c3e;
+            dic_control.c3e = event.data.c3e;
             let count = 0;
-            editor.eachLine(function(lineHandle) {
+            editor.eachLine(function (lineHandle) {
                 // Remove all classes from the line
                 editor.removeLineClass(count, 'wrap', 'line-decoration');
                 count++;
             });
-            setTimeout(function (){
+            setTimeout(function () {
                 esconde_tela_aguarde();
             }, 500);
             worker.terminate();
         }
-        if ('atualiza_tabela_variaveis' in event.data){
-            dic_control.historico_variavel =  event.data.atualiza_tabela_variaveis;
+        if ('atualiza_tabela_variaveis' in event.data) {
+            dic_control.historico_variavel = event.data.atualiza_tabela_variaveis;
             atualiza_tabela_variaveis(event.data.atualiza_tabela_variaveis);
         }
 
-        if ('le' in event.data){
-            setTimeout(function (){
+        if ('le' in event.data) {
+            setTimeout(function () {
                 esconde_tela_aguarde();
             }, 500);
-            setTimeout(function (){
+            setTimeout(function () {
                 document.getElementById('inputText').removeAttribute('disabled');
                 $("#inputText").addClass('input-insere-dados');
                 $("#inputText")[0].placeholder = `Digite um valor de entrada para variável '${event.data.var}'`;
@@ -208,12 +229,12 @@ function inicia_worker(debug=false){
             worker.postMessage(userInput);
         }
 
-        if ('debugger' in event.data){
-            setTimeout(function (){
+        if ('debugger' in event.data) {
+            setTimeout(function () {
                 esconde_tela_aguarde();
             }, 500);
             modifica_cor_linhas_editor_texto(event.data.linha_atual, event.data.linha_anterior);
-            if (event.data.debugger){
+            if (event.data.debugger) {
                 esta_no_debug = true;
                 await getUserDebug2();
                 esta_no_debug = false;
